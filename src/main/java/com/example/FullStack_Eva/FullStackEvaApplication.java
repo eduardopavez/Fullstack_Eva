460203b830eb06776e694b18497b3b2b77d1836c
Minillorsh1 <minillorsh@gmail.com>
2025-05-20 20:02:45 -0400

    primer commit

---
+++ src/main/java/com/example/FullStack_Eva/FullStackEvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullStack_Eva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackEvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackEvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FullStack_Eva/FullStackEvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullStack_Eva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackEvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
